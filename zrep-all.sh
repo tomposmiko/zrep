757f0d99e671b34d37a1cc00b3466a5cdbbbe34b
tompos <tpapp@chemaxon.com>
2019-04-21 22:10:05 +0200

    fix zrep.sh switch in zrep-all.sh

--- zrep-all.sh
+++ zrep-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export PATH="/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-echo "BEGIN: `date "+%Y-%m-%d %H:%M:%S"`"
 
 confdir="/etc/zrep"
 conffile="$confdir/zrep.conf"
 
+echo "BEGIN: `date "+%Y-%m-%d %H:%M:%S"`"
 for i in `grep -v ^\# $conffile`;do
 	echo "==== $i ===="
-	zrep.sh $i
+	zrep.sh -s $i
 done
 
 echo "END: `date "+%Y-%m-%d %H:%M:%S"`"
