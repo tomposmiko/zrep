4752e69885f99b794410f1cd1081b9cf6c97861b
Tamas Papp <2580497+tomposmiko@users.noreply.github.com>
2022-04-29 21:59:44 +0200

    Add lxdsr.sh

---
+++ lxdsr.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+interactive=0
+if /usr/bin/tty > /dev/null;
+  then
+    interactive=1
+fi
+
+# https://github.com/maxtsepkov/bash_colors/blob/master/bash_colors.sh
+uncolorize () { sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g"; }
+if [ $interactive -eq 1 ]
+  then say() { echo -ne "$1";echo -e "$nocolor"; }
+    # Colors, yo!
+    green="\e[1;32m"
+    # shellcheck disable=SC2034
+    red="\e[1;31m"
+    # shellcheck disable=SC2034
+    blue="\e[1;34m"
+    # shellcheck disable=SC2034
+    purple="\e[1;35m"
+    # shellcheck disable=SC2034
+    cyan="\e[1;36m"
+    nocolor="\e[0m"
+  else
+    # do nothing
+    say() { true; }
+fi
+
+export PATH="/root/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+dataset_list=$(mktemp /tmp/datasets.XXXXXX)
+zfs list -t snap -r -H tank -o name -s name tank/lxd/containers | grep @snapshot- | sed -e 's@tank/lxd/containers/@@' -e 's,@snapshot-,/,' > "$dataset_list"
+
+for snap in "$@";do
+  # shellcheck disable=SC2013
+  for dataset_path in $(grep "$snap" "$dataset_list");do
+    say "$green lxc delete ${dataset_path}"
+    lxc delete "${dataset_path}"
+  done
+done
+
+rm "${dataset_list}"
