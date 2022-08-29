ce07967cc6bfc923f9c73097f2502783434b5a7e
Tamas Papp <2580497+tomposmiko@users.noreply.github.com>
2022-08-29 22:32:58 +0200

    Refraktor zrep-check.sh
    - Add --noop
    - Add --debug
    - Add parameterized daily and weekly limits

--- zrep-check.sh
+++ zrep-check.sh
@@ -3,7 +3,7 @@
 export PATH="/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # shellcheck disable=SC1091
-. /etc/zabbix/userparams/config
+#. /etc/zabbix/userparams/config
 
 slack_url_post='https://slack.com/api/chat.postMessage'
 # https://api.slack.com/authentication/oauth-v2
@@ -11,115 +11,134 @@ slack_url_post='https://slack.com/api/chat.postMessage'
 #slack_channel='<channel>'
 . /etc/slack-notification.conf
 
-debug_mode=0
+f_check_switch_param(){
+  if echo x"$1" |grep -q ^x$;
+   then
+     say "$red Missing argument!"
+     exit 1
+  fi
+}
+
+
+f_usage(){
+  echo "Usage:"
+  echo " $0 -d|--daily <days> -w|--weekly <days> [ --debug ] [ --noop ]"
+  echo " $0 -h|--help"
+  echo
+  echo "-d|--daily <days>      set the alert limit for the last daily snapshot"
+  echo "-w|--weekly <days>     set the alert limit for the last weekly snapshot"
+  exit 1
+}
+
+# Exit if no arguments!
+(( $# )) || { f_usage; exit 1; }
+
+while [ "$#" -gt "0" ]; do
+  case "$1" in
+    -d|--daily)
+      PARAM=$2
+      f_check_switch_param "$PARAM"
+      limit_daily="$PARAM"
+      shift 2
+    ;;
+
+    -w|--weekly)
+      PARAM="$2"
+      f_check_switch_param "$PARAM"
+      limit_weekly="$PARAM"
+      shift 2
+    ;;
+
+    --noop)
+      noop_mode=1
+      shift 1
+    ;;
+
+    --debug)
+      debug_mode=1
+      shift 1
+    ;;
+
+    *)
+      f_usage
+    ;;
+
+   esac
+done
+
+date_today=$(date "+%Y-%m-%d")
+
 
 f_slack_post() {
   slack_text="$1"
   # shellcheck disable=SC2154
-  curl -d "text=$slack_text" -d "channel=$slack_channel" -d "token=$slack_token" -s -X POST $slack_url_post > /dev/null
+  if [ -z "$debug_mode" ];
+    then
+      #curl -d "text=$slack_text" -d "channel=$slack_channel" -d "token=$slack_token" -s -X POST $slack_url_post > /dev/null
+      echo "$slack_text"
+    else
+      echo "$slack_text"
+  fi
 }
 
-displaytime1() {
-  local T=$1
-  local D=$((T/60/60/24))
-  local H=$((T/60/60%24))
-  local M=$((T/60%60))
-  local S=$((T%60))
-  (( "$D" > 0 )) && printf '%d days ' $D
-  (( "$H" > 0 )) && printf '%d hours ' $H
-  (( "$M" > 0 )) && printf '%d minutes ' $M
-  (( "$D" > 0 || "$H" > 0 || "$M" > 0 )) && printf 'and '
-  printf '%d seconds\n' $S
+f_date_to_epoch () {
+  date --utc --date "$1" +%s
 }
 
-displaytime2() {
-  local t="$1"
-  local d=$((t/60/60/24))
-  local h=$((t/60/60%24))
-  local m=$((t/60%60))
-  local s=$((t%60))
-
-  if [[ $d -gt 0 ]];
-    then
-      [[ $d = 1 ]] && echo -n "$d day " || echo -n "$d days "
-  fi
-
-  if [[ $h -gt 0 ]];
-    then
-      [[ $h = 1 ]] && echo -n "$h hour " || echo -n "$h hours "
-  fi
-
-  if [[ $m -gt 0 ]];
-    then
-      [[ $m = 1 ]] && echo -n "$m minute " || echo -n "$m minutes "
-  fi
-
-  if [[ $d = 0 && $h = 0 && $m = 0 ]];
-    then
-      [[ $s = 1 ]] && echo -n "$s second" || echo -n "$s seconds"
-  fi
-  echo
+f_date_diff (){
+  case "$1" in
+    -s)   sec=1;      shift;;
+    -m)   sec=60;     shift;;
+    -h)   sec=3600;   shift;;
+    -d)   sec=86400;  shift;;
+    *)    sec=86400;;
+  esac
+  dte1=$(f_date_to_epoch "$1")
+  dte2=$(f_date_to_epoch "$2")
+  diffSec=$((dte2-dte1))
+  echo $((diffSec/sec))
 }
 
-if [ "$1" == "--debug" ];
+
+if [ -z "$debug_mode" ];
   then
-    readonly debug_mode=1
-fi
-
-# for debugging
-if [ $debug_mode -eq 1 ];
-  then
-    snap_list_file=a.txt
+    snap_list_file=$(mktemp /tmp/snap_list_file.XXXX)
+    zfs list -t all -r tank/zrep -o name -H | grep -v "^tank/zrep$" | tee "$snap_list_file" > /dev/null
   else
-    snap_list_file=$(mktemp /tmp/tmp.zrep_check_snap_list.XXXX)
-    zfs list -t all -r tank/zrep -o name -H | grep -v "^tank/zrep$" > "$snap_list_file"
+    snap_list_file=a.txt
 fi
 
 dataset_list=$(grep -v @ "$snap_list_file")
 
-#for i in $dataset_list
 f_check_late(){
   dataset="$1"
   freq="$2"
-  date_late="$3"
+  days_limit="$3"
 
   snap_last_item=$(grep "${dataset}@" "$snap_list_file" | grep -o "zas-${freq}-.*" | tail -1)
-  snap_last_date=$(echo "$snap_last_item" | grep -o "202[0-9]-[0-9][0-9]-[0-9][0-9]")
-  snap_last_epoch=$(date "+%s" -d "$snap_last_date")
+  date_last_snap=$(echo "$snap_last_item" | grep -o "202[0-9]-[0-9][0-9]-[0-9][0-9]")
+  days_diff=$(f_date_diff -d "$date_last_snap" "$date_today")
 
-  time_late_epoch=$(date -d "$date_late" +%s)
-  time_difference=$(( "$snap_last_epoch" - "$time_late_epoch" ))
-
-#date_2_days_ago=$(date -d "2 days ago" +%s)
-#echo $snap_last_epoch
-#echo $date_2_days_ago
-#echo $[ $snap_last_epoch - $date_2_days_ago ]
-
-
-  time_human=$(displaytime2 $time_difference)
-  if [ $debug_mode -eq 1 ];
+  if (( "$days_diff" > "$days_limit" ));
     then
-      echo "DEBUG: Time spent since ${freq} backup of ${dataset}: $time_human"
-      f_slack_post "DEBUG: Time spent since *${freq}* backup of *${dataset}*: *${time_human}*"
-
-    else
-      if [ "$time_difference" -le 0 ];
+      msg="Time spent since the last *${freq}* backup of *${dataset}*: *${days_diff} days*"
+      if [ -z "$debug_mode" ];
         then
-          echo "Time spent since ${freq} backup of ${dataset}: $time_human"
-          f_slack_post "Time spent since *${freq}* backup of *${dataset}*: *${time_human}*"
+          echo "$msg"
+          [[ -z "$noop_mode" ]] && f_slack_post "$msg"
+        else
+          echo "DEBUG: $msg"
+          [[ -z "$noop_mode" ]] && f_slack_post "DEBUG: $msg"
       fi
   fi
-
 }
 
 for d in $dataset_list;do
-  f_check_late "$d" daily "2 days ago";
+  f_check_late "$d" daily "$limit_daily";
 done
 
 for d in $dataset_list;do
-  f_check_late "$d" weekly "8 days ago";
+  f_check_late "$d" weekly "$limit_weekly";
 done
 
-rm -f "$snap_list_file"
-
-# zabbix_sender -z $SENDER_HOST -p $SENDER_PORT -i $TMPFILE $ZS_ARGS
+[[ -z "$debug_mode" ]] && rm -f "$snap_list_file"
