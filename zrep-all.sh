d0fa09d9897c73440c146082669b7818866b7c44
Tamas Papp <tamas.papp@rtfm.co.hu>
2023-07-01 00:50:38 +0200

    More cleanup

--- zrep-all.sh
+++ zrep-all.sh
@@ -16,7 +16,7 @@ f_process_args() {
             -c|--conf)
                 PARAM=$2
                 fc_check_arg "$PARAM" "config file"
-                conffile="$PARAM"
+                FILE_CONFIG="$PARAM"
                 shift 2
             ;;
 
@@ -34,6 +34,21 @@ f_process_args() {
     done
 }
 
+f_run_all() {
+    fc_say_info "BEGIN: $(date "+%Y-%m-%d %H:%M")"
+
+    #for source_item in $(grep -v "^#" "$conffile"); do
+    local source_item
+
+    while IFS= read -r source_item; do
+        DATE=$(date "+%Y-%m-%d %H:%M:%S")
+        fc_say_info "${DATE} - $source_item"
+        zrep.sh --quiet -s "$source_item" -f "$FREQ" -c "$FILE_CONFIG" || true
+    done <<< "$FILE_CONFIG"
+
+    fc_say_info "FINISH: $(date "+%Y-%m-%d %H:%M")"
+}
+
 f_usage(){
     echo "Usage:"
     echo "  $0 [-c conffile]"
@@ -48,14 +63,4 @@ f_usage(){
 f_process_args "${@}"
 f_validate_config_path
 f_validate_freq
-
-fc_say_info "BEGIN: $(date "+%Y-%m-%d %H:%M")"
-
-
-for source_item in $(grep -v "^#" "$conffile"); do
-    DATE=$(date "+%Y-%m-%d %H:%M:%S")
-    fc_say_info "${DATE} - $source_item"
-    zrep.sh --quiet -s "$source_item" -f "$FREQ" -c "$conffile" || true
-done
-
-fc_say_info "FINISH: $(date "+%Y-%m-%d %H:%M")"
+f_run_all
